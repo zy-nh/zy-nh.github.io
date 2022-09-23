c74b6edec164d5addac2ec852d3dbb77c4f07b4f
zy-nh <1205282944@qq.com>
2022-09-23 23:05:23 +0800

    modify config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -142,13 +142,13 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
-    repo: 'zy-nh', // 导航栏右侧生成Github链接
+    repo: 'zy-nh/zy-nh.github.io', // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: '上次更新', // 开启更新时间，并配置前缀文字   string | boolean (取值为git提交时间)
     docsDir: 'docs', // 编辑的文件夹
     // docsBranch: 'master', // 编辑的文件所在分支，默认master。 注意：如果你的分支是main则修改为main
     editLinks: true, // 启用编辑
-    editLinkText: '编辑',
+    editLinkText: '欢迎纠错',
 
     //*** 以下是Vdoing主题相关配置，文档：https://doc.xugaoyi.com/pages/a20ce8/ ***//
 
