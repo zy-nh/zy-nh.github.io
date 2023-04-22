00b21ee658a287b0a0c02b844df250b5f3e052d9
zy-nh <1205282944@qq.com>
2023-04-22 22:48:35 +0800

    update dist

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # 确保脚本抛出遇到的错误
 set -e
 npm run build # 生成静态文件
-cd docs/.vuepress/dist # 进入生成的文件夹
+# cd docs/.vuepress/dist # 进入生成的文件夹
 
 # deploy to github
 echo 'zhuyee.cn' > CNAME
@@ -15,10 +15,10 @@ else
   git config --global user.name "zy-nh"
   git config --global user.email "1205282944@qq.com"
 fi
-git init
+# git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl master:gh-pages # 推送到github
+git push -f $githubUrl master #:gh-pages # 推送到github
 
-cd -
-rm -rf docs/.vuepress/dist
+# cd -
+# rm -rf docs/.vuepress/dist
